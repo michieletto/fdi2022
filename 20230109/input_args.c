5f365b0b21ed97752be15d9469b3dd1f8963e3f9
Stefano Michieletto <stefano.michieletto@unipd.it>
2023-01-12 02:24:49 +0100

    09/01/2023

---
+++ 20230109/input_args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    for (int i = 0; i < argc; i++) {
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
+
+    return 0;
+}
